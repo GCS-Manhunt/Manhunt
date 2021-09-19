1326941965b0875baca917c91865d376c84e0487
Aehmttw <mateibudiu@hotmail.com>
2021-09-19 13:33:08 -0400

    Initial LibGDX project

---
+++ desktop/src/manhuntgame/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package manhuntgame.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import manhuntgame.app.ManhuntGameApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ManhuntGameApp(), config);
+	}
+}
