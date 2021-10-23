093569abeb9d7839257c4d3576eb99db8cd59a47
Yoseph <yosephmak03@gmail.com>
2021-10-22 20:51:57 -0400

    modified main screen + moved previous main screen to debug (temporary button)

---
+++ core/src/manhuntgame/ui/screen/ScreenBeginGame.java
@@ -0,0 +1,63 @@
+package manhuntgame.ui.screen;
+
+import manhuntgame.ui.Button;
+import manhuntgame.app.App;
+import manhuntgame.app.Drawer;
+
+public class ScreenBeginGame extends Screen
+{
+    /*
+    a) create game
+    b) join game
+    c) back to main screen
+     */
+
+    Button create = new Button(540, 1000, 900, 200, "Create", new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            App.app.screen = new ScreenDebug(); // can be changed later
+        }
+    });
+
+    Button join = new Button(540, 1250, 900, 200, "Join/Connect", new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            App.app.screen = new ScreenDebug(); // can be changed later
+        }
+    });
+
+    Button back = new Button(540, 1500, 900, 200, "Back", new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            App.app.screen = new ScreenMain();
+        }
+    });
+
+    @Override
+    public void update()
+    {
+        // update buttons and not much else
+        create.update();
+        join.update();
+        back.update();
+    }
+
+    @Override
+    public void draw()
+    {
+        create.draw();
+        join.draw();
+        back.draw();
+
+        Drawer drawer = App.app.drawer;
+        drawer.setFontSize(120);
+        drawer.drawText(60, 100, "Manhunt Game", false);
+
+    }
+}
