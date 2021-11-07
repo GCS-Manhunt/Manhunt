e768d13fff64e293094261da0aa25ec8d902674c
Alan Abraham <abrahamalan2@gmail.com>
2021-11-07 16:40:55 -0500

    added EventCodeConfirmation

---
+++ core/src/manhuntgame/network/event/EventCodeConfirmation.java
@@ -0,0 +1,33 @@
+package manhuntgame.network.event;
+
+import io.netty.buffer.ByteBuf;
+import manhuntgame.network.NetworkUtils;
+
+public class EventCodeConfirmation extends PersonalEvent{
+
+    String confirmationString;
+
+    public EventCodeConfirmation(String confirmationString) {
+        this.confirmationString = confirmationString;
+    }
+
+    public EventCodeConfirmation() {
+
+    }
+
+    @Override
+    public void write(ByteBuf b) {
+        NetworkUtils.writeString(b, this.confirmationString);
+    }
+
+    @Override
+    public void read(ByteBuf b) {
+        NetworkUtils.writeString(b, this.confirmationString);
+    }
+
+    @Override
+    public void execute() {
+        //For Matei
+    }
+}
+
